77b84e27793c528b2b9c8f99660ee655ff65412d
Mariusz Dziemianowicz <mariusz@dziemianowi.cz>
2019-10-16 19:14:48 +0200

    codenames: update shell script

--- codenames/util/random.sh
+++ codenames/util/random.sh
@@ -2,6 +2,6 @@
 BASEDIR=$(dirname "$0")
 for i in $(seq 1 $1)
 do
-    shuf -n 1 $BASEDIR/../static/adjectives-verbs.txt | tr -d '\r\n' | awk '{printf $1" "}' && \
-    shuf -n 1 $BASEDIR/../static/nouns.txt;
+    shuf -n 1 $BASEDIR/../assets/adjectives-verbs.txt | tr -d '\r\n' | awk '{printf $1" "}' && \
+    shuf -n 1 $BASEDIR/../assets/nouns.txt;
 done
